f72b2b5364c724f7c51dc99f0cc774a6a41c5a61
Punit <Punit@test.local>
2021-03-25 20:49:21 +0530

    Added additional keywords

--- src/testscript/Keywords.java
+++ src/testscript/Keywords.java
@@ -3,6 +3,7 @@ package testscript;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -16,6 +17,7 @@ public class Keywords {
 	public void openbrowser() throws Exception {
 			System.setProperty("webdriver.chrome.driver","E:\\Project_Zoho\\JARS\\chromedriver.exe" ); 
 			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			file = new FileInputStream("F:\\punit_java_eclipse\\CRMframeworkZoho\\src\\objectrepository\\objectrepository.properties");
 			prop = new Properties();
 			prop.load(file);
@@ -26,6 +28,11 @@ public class Keywords {
 		}
 	public void input(String data, String objectName) {
 		driver.findElement(By.xpath(prop.getProperty(objectName))).sendKeys(data);
+		
+		
+	}
+	public void click(String objectName) {
+		driver.findElement(By.xpath(prop.getProperty(objectName))).click();
 		
 		
 	}
